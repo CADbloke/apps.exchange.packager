f326f60c8cea077c2b1bd8bfbecde5bac5a4f2cb
Ewen <Ewen@CADbloke.com>
2019-04-26 16:22:49 +1000

    verbose logging + suppress Light.exe ICE80 error

--- scripts/build-win-sub.bat
+++ scripts/build-win-sub.bat
@@ -1,11 +1,11 @@
 rem @echo off
 
-%DEVBIN%\bin\candle.exe .\temp\root.wxs -out .\temp\plugin.wixobj
+%DEVBIN%\bin\candle.exe .\temp\root.wxs -out .\temp\plugin.wixobj -v
 if ERRORLEVEL 1 (
 	echo "Wix/Candle error - Build aborted!"
 	goto end
 ) else (
-	%DEVBIN%\bin\light.exe -sw1076 -ext WixNetFxExtension .\temp\plugin.wixobj -out ".\output\%AppName%-win64.msi"
+	%DEVBIN%\bin\light.exe -sw1076 -ext WixNetFxExtension .\temp\plugin.wixobj -out ".\output\%AppName%-win64.msi" -v -sice:ICE80
 	if ERRORLEVEL 1 (
 		echo "Wix/Light error - Build aborted!"
 		goto end
