25e55c2430893d679baf64dafbe5df9853b13e85
Ewen <Ewen@CADbloke.com>
2019-07-21 11:48:04 +1000

    deleted unused profiles and things

--- profiles/maxPerMachineIcon/BootstrapperMaker.bat
+++
@@ -1,6 +0,0 @@
-
-mkdir bootstrapper
-cd bootstrapper
-%DEVBIN%\bin\candle.exe ..\net40.wxs -ext WixBalExtension -ext %DEVBIN%\bin\WiXUtilExtension.dll -out ..\net40.wixobj
-%DEVBIN%\bin\light.exe ..\net40.wixobj -ext WixBalExtension
-cd ..
